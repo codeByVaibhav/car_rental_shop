6484ad6da96a9611c40fa1fb3a2d1586b16ba392
Vaibhav Shrma <k.vaibhav9013@gmail.com>
2020-07-21 23:20:58 +0530

    added loader

--- client/src/components/car-list.component.js
+++ client/src/components/car-list.component.js
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from 'react-loader-spinner';
 
-const Car = props => {
+const Car = props => (
+  <tr>
+    <td>{props.car.name}</td>
+    <td>{props.car.type}</td>
+    <td>{props.car.rented ? "Yes" : "No"}</td>
+    <td>
+      {props.car.rented ?
+        <button type="button" className="btn btn-dark" to={"/rent/" + props.car._id} disabled>Rent</button>
+        :
+        <Link type="button" className="btn btn-dark" to={"/rent/" + props.car._id}>Rent</Link>}
+    </td>
+  </tr>
+);
 
-  return (
-    <tr>
-      <td>{props.car.name}</td>
-      <td>{props.car.type}</td>
-      <td>{props.car.rented ? "Yes" : "No"}</td>
-      <td>
-        {props.car.rented ?
-          <button type="button" className="btn btn-dark" to={"/rent/" + props.car._id} disabled>Rent</button>
-          :
-          <Link type="button" className="btn btn-dark" to={"/rent/" + props.car._id}>Rent</Link>}
-      </td>
-    </tr>
-  );
-}
 
 export default class CarList extends Component {
 
@@ -37,7 +37,7 @@ export default class CarList extends Component {
     const cars_list = this.state.cars.map(car => {
       return <Car car={car} rentCar={this.deleteExercise} key={car._id} />;
     });
-    return (
+    return cars_list[0] ? (
       <div style={{ "width": "auto" }}>
         <h3>Cars List</h3>
         <table className="table">
@@ -55,5 +55,14 @@ export default class CarList extends Component {
         </table>
       </div>
     )
+      : (
+        <Loader className="center" style={{ "margin-top": "150px" }}
+          type="Puff"
+          color="#00BFFF"
+          height={100}
+          width={100}
+          timeout={5000} //5 secs
+        />
+      );
   }
 }
